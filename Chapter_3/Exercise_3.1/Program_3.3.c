fd58f6cfcb31575b6ae09977d380fab1f758c1ae
Wesley Taylor-Rendal <wtr@fpgamason.com>
2021-02-13 10:50:31 +0000

    Added program 3.3.c

---
+++ Chapter_3/Exercise_3.1/Program_3.3.c
@@ -0,0 +1,23 @@
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//    ____ ___   __    _               
+//   / __// o |,'_/  .' \              
+//  / _/ / _,'/ /_n / o /   _   __  _    ___  _   _  __
+// /_/  /_/   |__,'/_n_/   / \,' /.' \ ,' _/,' \ / |/ /
+//                        / \,' // o /_\ `./ o // || / 
+//                       /_/ /_//_n_//___,'|_,'/_/|_/ 
+//  
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// Author              : Wesley Taylor-Rendal (WTR)
+// Design history      : Review git logs.
+// Description         : Displaying multiple lines
+// Concepts introduced : printf function reacts to special character strings
+//                     : as shown by line breaks with \n
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Testing....\n..1\n...2\n....3\n");
+    return 0;
+}
+
