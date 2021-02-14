79c1cc94d51330ae47c1511b07728770ebb75f66
Wesley Taylor-Rendal <wtr@fpgamason.com>
2021-02-14 15:41:51 +0000

    Added Exercise 5.10

---
+++ Chapter_5/Exercise_5.10.c
@@ -0,0 +1,18 @@
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//    ____ ___   __    _               
+//   / __// o |,'_/  .' \              
+//  / _/ / _,'/ /_n / o /   _   __  _    ___  _   _  __
+// /_/  /_/   |__,'/_n_/   / \,' /.' \ ,' _/,' \ / |/ /
+//                        / \,' // o /_\ `./ o // || / 
+//                       /_/ /_//_n_//___,'|_,'/_/|_/ 
+//  
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// Author         : Wesley Taylor-Rendal (WTR)
+// Design history : Review git logs.
+// Description    : See what happens on program 5.8 if you input a negative
+// Concepts       : Modulus and integer division dealing with negatives.
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// Answer : 
+//        : The '-' is retained and repeated, because the output number each 
+//        : time from the modulus calculations is a negative. So each digit is 
+//        : "negative"
