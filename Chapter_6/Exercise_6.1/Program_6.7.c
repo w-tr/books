f2fdba20e824f389c9f4d5e4b80635f6a5b30df5
Wesley Taylor-Rendal <wtr@fpgamason.com>
2021-02-15 13:25:57 +0000

    Added program 6.7, prelude to switch-case statement

---
+++ Chapter_6/Exercise_6.1/Program_6.7.c
@@ -0,0 +1,34 @@
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//    ____ ___   __    _               
+//   / __// o |,'_/  .' \              
+//  / _/ / _,'/ /_n / o /   _   __  _    ___  _   _  __
+// /_/  /_/   |__,'/_n_/   / \,' /.' \ ,' _/,' \ / |/ /
+//                        / \,' // o /_\ `./ o // || / 
+//                       /_/ /_//_n_//___,'|_,'/_/|_/ 
+//  
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// Author         : Wesley Taylor-Rendal (WTR)
+// Design history : Review git logs.
+// Description    : Categorizing a Single Character Entered at the Terminal
+// Concepts       : scanf, with char data type
+//                : compounded checks on if branch, for upcase/lower, char or ..
+//                : Prelude to switch-case statement
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#include <stdio.h>
+
+int main(void)
+{
+    char c;
+
+    printf("Enter a SINGLE character: ");
+    scanf("%c", &c);
+
+    if ( (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') ) 
+        printf("It's an alphabetic character.\n");
+    else if (c >= '0' &&  c <= '9')
+        printf("It's a digit character.\n");
+    else
+        printf("It's a special character.\n");
+
+    return 0;
+}
