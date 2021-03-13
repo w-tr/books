d072529fe40b6e461a9c79a1ae06af2ee6f1f3ae
Wesley Taylor-Rendal <wtr@fpgamason.com>
2021-03-13 12:20:20 +0000

    Added Exercise 9.3, but what happens when dealing with 00 minutes and other use cases, such as midnight?

---
+++ Chapter_9/Exercise_9.3.c
@@ -0,0 +1,68 @@
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//    ____ ___   __    _               
+//   / __// o |,'_/  .' \              
+//  / _/ / _,'/ /_n / o /   _   __  _    ___  _   _  __
+// /_/  /_/   |__,'/_n_/   / \,' /.' \ ,' _/,' \ / |/ /
+//                        / \,' // o /_\ `./ o // || / 
+//                       /_/ /_//_n_//___,'|_,'/_/|_/ 
+//  
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// Author         : Wesley Taylor-Rendal (WTR)
+// Design history : Review git logs.
+// Description    : Find the elapsed time between t1, t2.
+// Concepts       : 
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#include <stdio.h>
+
+struct sTime
+{
+    int hour;      
+    int minutes;
+    int seconds;
+};
+
+struct sTime getTime(void)
+{
+    struct sTime theTime;
+    printf("Enter a time (hh:mm:ss) : \n");
+    scanf("%d:%d:%d", &theTime.hour, &theTime.minutes, &theTime.seconds);
+    return theTime;
+}
+    
+struct sTime elapse_time(struct sTime t1, struct sTime t2)
+{
+    struct sTime tOut;
+    if (t1.seconds > t2.seconds)
+    {
+        t2.seconds+=60;
+        t2.minutes--;
+    }
+    tOut.seconds = t2.seconds - t1.seconds;
+    if (t1.minutes > t2.minutes)
+    {
+        t2.minutes+=60;
+        t2.hour--;
+    }
+    tOut.minutes = t2.minutes - t1.minutes;
+    if (t1.hour > t2.hour)
+    {
+        t2.hour+=24;
+    }
+    tOut.hour = t2.hour - t1.hour;
+
+    return tOut;
+}
+
+
+int main(void)
+{
+    struct sTime t1, t2, t3;
+    struct sTime elapse_time(struct sTime t1, struct sTime t2);
+    struct sTime getTime(void);
+
+    t1 = getTime();
+    t2 = getTime();
+    t3 = elapse_time(t1, t2);
+    printf("Time difference is : %.2i:%.2i:%.2i\n", t3.hour, t3.minutes, t3.seconds);
+    return 0;
+}
